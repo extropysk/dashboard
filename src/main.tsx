81281ac6f5b107750f2fb87ec2b6ec65cdb67e70
Kristian Duda <kristian.duda@extropy.sk>
2025-07-27 20:22:42 +0200

    add toaster

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,27 @@ import { RouterProvider } from "react-router-dom"
 import { router } from "@/app"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-import "./index.css"
+import { toast } from "sonner"
 
-const queryClient = new QueryClient()
+import "./index.css"
+import { Toaster } from "@/components/ui/sonner"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      onError: (error) => {
+        toast.error(error.message)
+      },
+    },
+  },
+})
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster />
     </QueryClientProvider>
   </StrictMode>
 )
